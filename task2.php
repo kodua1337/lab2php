3a3d72c9740679b9b2ff7636bed8127177596b6e
kodua1337 <kn1b22.kodua@kpnu.edu.ua>
2025-06-22 15:21:45 +0300

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,11 @@
+﻿<?php
+// Завдання 2: Робота зі змінними
+$number = 123;                 // Число
+$string = strval($number);    // Перетворення на рядок
+$combined = "Результат: " . $string; // Об'єднання рядків
+$type = gettype($string);     // Визначення типу
+
+echo "<h2>Завдання 2</h2>";
+echo $combined . "<br>";
+echo "Тип змінної \$string: $type";
+?>
